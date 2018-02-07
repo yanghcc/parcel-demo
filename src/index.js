7a085fb7b04b74685544cbcdc62ad7bbd70a0186
yanghucheng@021.com <yanghucheng@021.com>
2018-02-07 15:27:30 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// import Tesseract from 'tesseract.js'
+import a from './js/main';
+console.log("123")
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import classes from '../css/index.styl';
+
+export default () => {
+  console.log(classes.main);
+};
